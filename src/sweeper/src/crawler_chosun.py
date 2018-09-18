de1ac9b69cddd07b3638be48f2180912ec936cfa
Shin Jinwoo <bluesky0255@naver.com>
2018-09-18 15:03:02 +0900

    EDIT 조선일보 crawler
    - Changed some variable names as convention.

--- src/sweeper/src/crawler_chosun.py
+++ src/sweeper/src/crawler_chosun.py
@@ -8,26 +8,26 @@ def extract_news(driver, url):
     # extract to Article instance
 
     # url에서 uid 추출
-    p = re.compile("/[0-9]+.html")
-    uid = p.search(url).group().split(".")[0].split("/")[1]
+    pattern = re.compile("/[0-9]+.html")
+    uid = pattern.search(url).group().split(".")[0].split("/")[1]
 
     driver.get(url)
     title = driver.find_element_by_id("news_title_text_id").text
-    dateText = driver.find_element_by_class_name("news_date").text
+    date_text = driver.find_element_by_class_name("news_date").text
     paragraphs = driver.find_elements_by_class_name("par")
     written_date = datetime.now()
 
-    pTexts = []
+    paragraph_texts = []
 
     for p in paragraphs:
-        pTexts.append(p.text)
+        paragraph_texts.append(p.text)
 
-    content = "\n\n".join(pTexts)
+    content = "\n\n".join(paragraph_texts)
 
     # ex. "입력 2018.09.18 03:01"
-    dateTokens = dateText.split("|")[0].split(" ")
-    date = dateTokens[1].split(".")
-    time = dateTokens[2].split(":")
+    date_tokens = date_text.split("|")[0].split(" ")
+    date = date_tokens[1].split(".")
+    time = date_tokens[2].split(":")
 
     written_date = written_date.replace(
         year=int(date[0]),
@@ -46,31 +46,26 @@ def extract_news(driver, url):
                    written_date=written_date)
 
 
-
-def extract_biz(driver, url):
-    # extract to Article instance
-    a = 1
-
-
 main_url = "http://www.chosun.com/"
-include_urls = []
 link_list = []
-
-include_urls.append("news.chosun.com")
 
 driver = wd.Chrome(executable_path="../webdriver/chromedriver.exe")
 driver.implicitly_wait(10)
 driver.get(main_url)
 
-article_link_pattern = "chosun.com/site/data/html_dir/"
+article_link_patterns = [
+    "news.chosun.com/site/data/html_dir/"
+]
 
 hrefElms = driver.find_elements_by_css_selector("[href]")
 print("%d elms found" % len(hrefElms))
 
 for i in hrefElms:
     href = i.get_attribute("href")
-    if article_link_pattern in href:
-        link_list.append(href)
+    for p in article_link_patterns:
+        if p in href:
+            link_list.append(href)
+            break
 
 print("%d articles found" % len(link_list))
 
