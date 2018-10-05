f04e260589ecd776ce454db5c23666006296eaa7
Shin Jinwoo <bluesky0255@naver.com>
2018-10-05 22:38:11 +0900

    ADD api gateway init stubs.

---
+++ src/microservice/user-api/src/main/kotlin/kr/latera/sibylla/userapi/config/SecurityConfig.kt
@@ -0,0 +1,17 @@
+package kr.latera.sibylla.userapi.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig: WebSecurityConfigurerAdapter() {
+    override fun configure(web: WebSecurity?) {
+        super.configure(web)
+        web?: return
+        web.ignoring()
+                .antMatchers("/**")
+    }
+}
---
+++ src/microservice/user-api/src/main/kotlin/kr/latera/sibylla/userapi/dto/UserAPIRequestDto.kt
@@ -0,0 +1,4 @@
+package kr.latera.sibylla.userapi.dto
+
+data class UserAPIRequestDto(val email: String?,
+                             val password: String?)
