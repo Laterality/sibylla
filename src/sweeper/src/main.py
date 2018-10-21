785f4e9a7de5540743c992d4045fe2be9b9c7fe6
Shin Jinwoo <bluesky0255@naver.com>
2018-10-21 10:48:33 +0900

    EDIT sweeper to send webhook after crawling articles finished

--- src/sweeper/src/main.py
+++ src/sweeper/src/main.py
@@ -1,7 +1,12 @@
 import crawler_joongang
 import crawler_chosun
 import crawler_donga
+import util
 
 crawler_joongang.crawl()
 # crawler_chosun.crawl()
 # crawler_donga.crawl()
+
+# After crawling finished,
+# Send webhook to prophet to train with crawled articles
+util.send_webhook()
--- src/sweeper/src/util.py
+++ src/sweeper/src/util.py
@@ -6,7 +6,8 @@ import sys
 
 
 def get_driver():
-    DRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH")
+    # DRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH")
+    DRIVER_PATH = "../webdriver/chromedriver.exe"
 
     if DRIVER_PATH is None:
         print("Invalid chrome driver path: %s" % DRIVER_PATH)
@@ -20,7 +21,6 @@ def get_driver():
 
 
 def post(article, source_name):
-    REST_URL = "http://172.18.0.4/api/article"
     REST_URL = "http://altair.latera.kr/sb/api/article"
 
     data = {
@@ -35,7 +35,16 @@ def post(article, source_name):
     headers = {
         "Content-Type": "application/json; charset=utf-8"
     }
+
     res = requests.post(url=REST_URL, data=json.dumps(data), headers=headers)
 
     if res.status_code != 201:
         print("Request not handled, url: %s" % article.url)
+
+
+def send_webhook():
+    REST_URL = "http://altair.latera.kr/sb/prophet"
+
+    res = requests.get(url=REST_URL + "/train")
+    if res.status_code != 200:
+        print("Webhook failed")
