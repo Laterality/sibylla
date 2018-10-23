92ed5fda8d8d6f56629dc09b621523a31d379238
Shin Jinwoo <bluesky0255@naver.com>
2018-10-23 13:38:05 +0900

    EDIT microservice dockerfile

---
+++ src/microservice/auth-api/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+chmod +x ./gradlew
+./gradlew && java -jar ./build/libs/auth-api-0.0.1-SNAPSHOT.jar
---
+++ src/microservice/user-api/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+chmod +x ./gradlew
+./gradlew && java -jar ./build/libs/user-api-0.0.1-SNAPSHOT.jar
