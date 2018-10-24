91b2381f0d16e889e38b6e8e3a2102544d10903b
Shin Jinwoo <bluesky0255@naver.com>
2018-10-24 11:23:55 +0900

    EDIT view home page call api

--- src/microservice/sibylla-view/src/page/Home.tsx
+++ src/microservice/sibylla-view/src/page/Home.tsx
@@ -1,4 +1,9 @@
 import * as React from "react";
+import { AxiosResponse } from "axios";
+
+import { default as Api } from "../lib/Api";
+
+import { default as Article } from "../lib/Article";
 
 import { default as Header } from "../component/Header";
 import { default as Content } from "../component/Content";
@@ -7,7 +12,19 @@ interface IHomePageComponentProps {
     location: Location;
 }
 
-export default class HomePageComponent extends React.Component<IHomePageComponentProps> {
+interface IHomePageComponentState {
+    articles: Array<Article>;
+}
+
+export default class HomePageComponent extends React.Component<IHomePageComponentProps, IHomePageComponentState> {
+
+    constructor(props: IHomePageComponentProps) {
+        super(props);
+
+        this.state = {
+            articles: [],
+        };
+    }
 
     render() {
         const style = {
@@ -17,8 +34,28 @@ export default class HomePageComponent extends React.Component<IHomePageComponen
         return (
             <div style={style}>
                 <Header/>
-                <Content/>
+                <Content articles={this.state.articles}/>
             </div>
         );
+    }
+
+    componentDidMount() {
+        // Get article list
+        Api.retrieveArticles(21)
+        .then((res: AxiosResponse) => {
+            const articles = [];
+
+            for (let a of res.data["articles"]) {
+                articles.push(new Article(
+                    a["id"], 
+                    a["title"],
+                    a["content"],
+                    a["sourceName"],
+                    new Date(a["writtenDate"]),
+                    a["url"],
+                    a["images"]));
+            }
+            this.setState({articles});
+        });
     }
 }
