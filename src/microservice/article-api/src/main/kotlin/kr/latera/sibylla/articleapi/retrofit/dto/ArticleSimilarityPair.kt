1e6e94c6efb176f373e8b99d10cb70e640e79aa9
Shin Jinwoo <bluesky0255@naver.com>
2018-11-02 09:51:20 +0900

    EDIT article api recommendation list order change

--- src/microservice/article-api/src/main/kotlin/kr/latera/sibylla/articleapi/retrofit/dto/ArticleSimilarityPair.kt
+++ src/microservice/article-api/src/main/kotlin/kr/latera/sibylla/articleapi/retrofit/dto/ArticleSimilarityPair.kt
@@ -4,8 +4,8 @@ data class ArticleSimilarityPair(val articleId: Long,
                                  val similarity: Double) : Comparable<ArticleSimilarityPair> {
     override fun compareTo(other: ArticleSimilarityPair): Int {
         return when {
-            similarity < other.similarity -> -1
-            similarity > other.similarity -> 1
+            similarity < other.similarity -> 1 // -1
+            similarity > other.similarity -> -1 // 1
             else -> 0
         }
     }
