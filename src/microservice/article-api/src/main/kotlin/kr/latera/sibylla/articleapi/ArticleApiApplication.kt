32546ca2d7c991084fa735e2acec1aefad0eb136
Shin Jinwoo <bluesky0255@naver.com>
2018-11-07 13:56:22 +0900

    ADD bean

--- src/microservice/article-api/src/main/kotlin/kr/latera/sibylla/articleapi/ArticleApiApplication.kt
+++ src/microservice/article-api/src/main/kotlin/kr/latera/sibylla/articleapi/ArticleApiApplication.kt
@@ -2,10 +2,17 @@ package kr.latera.sibylla.articleapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 
 @SpringBootApplication
 class ArticleApiApplication
 
 fun main(args: Array<String>) {
     runApplication<ArticleApiApplication>(*args)
+}
+
+@Bean
+fun propertySourcePlaceholderConfiguerer: PropertySourcesPlaceholderConfigurer {
+    return PropertySourcesPlaceholderConfigurer()
 }
