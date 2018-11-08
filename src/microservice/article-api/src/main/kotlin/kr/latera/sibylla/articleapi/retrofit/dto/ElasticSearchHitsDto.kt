b5273b94e0368f2f385516b225a83d9ff95f49b9
Shin Jinwoo <bluesky0255@naver.com>
2018-11-08 13:29:59 +0900

    EDIT type Array -> List

--- src/microservice/article-api/src/main/kotlin/kr/latera/sibylla/articleapi/retrofit/dto/ElasticSearchHitsDto.kt
+++ src/microservice/article-api/src/main/kotlin/kr/latera/sibylla/articleapi/retrofit/dto/ElasticSearchHitsDto.kt
@@ -2,4 +2,4 @@ package kr.latera.sibylla.articleapi.retrofit.dto
 
 data class ElasticSearchHitsDto<T>(val total: Int,
                                 val max_score: Double,
-                                val hits: Array<T>)
+                                val hits: List<T>)
