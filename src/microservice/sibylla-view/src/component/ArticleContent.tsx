01685617b0b81191c734859093bcae57caee03bd
Shin Jinwoo <bluesky0255@naver.com>
2018-11-15 13:53:53 +0900

    EDIT view's component style

--- src/microservice/sibylla-view/src/component/ArticleContent.tsx
+++ src/microservice/sibylla-view/src/component/ArticleContent.tsx
@@ -62,7 +62,7 @@ export default class ArticleContentPageComponent extends React.Component<IArticl
             now.getUTCDate() !== writtenDate.getUTCDate();
         const writtenDateStr = `${dspYear? writtenDate.getUTCFullYear() + ". " : ""}${dspMonthDay? writtenDate.getMonth() + ". " + writtenDate.getUTCDate() + ". " : ""}${writtenDate.getUTCHours()}:${writtenDate.getUTCMinutes()}`;
         
-        return (<div>
+        return (<div id="content">
             <h2 className="my-3">{article.title}</h2>
             <div className="article-meta"><img src="./img/joongang_logo_circle.png"/><h6>{article.sourceName}</h6><h6>| {writtenDateStr}</h6><a href={article.url} className="goto-source">원문 보기</a></div>
             <p className="my-3" style={style.content}>{article.content}</p>
