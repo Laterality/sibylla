aed2c95489fbfe741b576e7bd7207ff256d78c17
Shin Jinwoo <bluesky0255@naver.com>
2018-11-18 22:10:41 +0900

    EDIT view search result

--- src/microservice/sibylla-view/src/component/ArticleListItem.tsx
+++ src/microservice/sibylla-view/src/component/ArticleListItem.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 import { default as Article } from "../lib/Article";
+import Routes from "../lib/Routes";
 
 interface IArticleListItemComponentProps {
     article: Article;
@@ -19,7 +21,7 @@ export default class ArticleListItemComponent extends React.Component<IArticleLi
         const writtenDateStr = `${dspYear? writtenDate.getUTCFullYear() + ". " : ""}${dspMonthDay? writtenDate.getMonth() + ". " + writtenDate.getUTCDate() + ". " : ""}${writtenDate.getUTCHours()}:${writtenDate.getUTCMinutes()}`;
         
         return (<div className="article-list-item">
-            <h2 onClick={() => this.props.onClick(props.article.id)}>{props.article.title}</h2>
+            <Link to={Routes.ROUTE_ARTICLE_CONTENT + "?id=" + props.article.id} onClick={() => this.props.onClick(props.article.id)}><h2>{props.article.title}</h2></Link>
             <div className="article-meta"><img src="./img/joongang_logo_circle.png"/><h6>{props.article.sourceName}</h6><h6>| {writtenDateStr}</h6><a href={props.article.url} className="goto-source">원문 보기</a></div>
             <p>{props.article.content.slice(0, 250) + "..."}</p>
         </div>);
