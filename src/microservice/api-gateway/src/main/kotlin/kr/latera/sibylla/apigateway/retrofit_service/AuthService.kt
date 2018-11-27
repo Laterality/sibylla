c49a828e269818a3af444d9f7a8d7d823ade1da2
Shin Jinwoo <bluesky0255@naver.com>
2018-11-27 17:01:27 +0900

    EDIT api gateway missed code

--- src/microservice/api-gateway/src/main/kotlin/kr/latera/sibylla/apigateway/retrofit_service/AuthService.kt
+++ src/microservice/api-gateway/src/main/kotlin/kr/latera/sibylla/apigateway/retrofit_service/AuthService.kt
@@ -2,9 +2,11 @@ package kr.latera.sibylla.apigateway.retrofit_service
 
 import kr.latera.sibylla.apigateway.dto.ResponseDto
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Header
 
 interface AuthService {
 
+    @GET("verify")
     fun verify(@Header("Authorization") authHeader: String): Call<ResponseDto<Any>>
 }
